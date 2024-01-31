1d0a00f2c033e38ed8fb473d5fedb15fe249a67e
Jerannn <145209442+Jerannn@users.noreply.github.com>
2024-01-31 13:26:36 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
 import Header from "./Header";
 import ItemList from "./ItemList";
 import SideBar from "./SideBar";
+import { initialItems } from "./constants";
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
+
   return (
     <>
       <BackgroundHeading />
       <main>
         <Header />
-        <ItemList />
-        <SideBar />
+        <ItemList items={items}/>
+        <SideBar setItems={setItems}/>
       </main>
 
       <Footer />

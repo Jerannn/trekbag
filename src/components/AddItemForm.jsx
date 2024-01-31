1074a6b499c7393823930a7ac0a635b017e7469e
Jerannn <145209442+Jerannn@users.noreply.github.com>
2024-01-31 13:25:26 +0800

    Update AddItemForm.jsx

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import Button from "./Button";
 
-export default function AddItemForm() {
+export default function AddItemForm({setItems}) {
+
+  const [item, setItem] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const newItem = {
+      id: Date.now(),
+      name: item,
+      packed: false,
+    }
+    setItems((prevItems) => [...prevItems, newItem]);
+    
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input />
-      <Button>Add to list</Button>
+      <input value={item} onChange={(e) => setItem(e.target.value)}/>
+      <Button>Add to lis</Button>
     </form>
   );
 }
